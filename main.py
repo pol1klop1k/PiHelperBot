deecd960a678942c3fee6f3c2a866f1b02d75f9a
pol1klop1k <garik.b.04@gmail.com>
2022-11-09 17:45:39 +0300

    set webhook

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.executor import start_webhook
 import config
 import os
 
-
+#env vars
 TOKEN = os.getenv('BOT_TOKEN')
+
+#webhook settings
+WEBHOOK_HOST = 'https://pihelperbot.onrender.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+
+#webserver setitings
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+
 
 
 @dp.message_handler(commands=['help'])
@@ -25,4 +47,12 @@ async def echo(message: types.Message):
     await message.answer(text=message.text)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
