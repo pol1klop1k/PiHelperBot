8279dfaabc779faf276b278a1f5c29a6935dc708
pol1klop1k <garik.b.04@gmail.com>
2022-11-09 18:30:13 +0300

    try on render

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 
 #env vars
 TOKEN = os.getenv('BOT_TOKEN')
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+#HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 #webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = 'https://pihelperbot.onrender.com'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
