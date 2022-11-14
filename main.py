8f511f1708429666bb8796e7bf85442290e6ebed
unknown <garik.b.04@gmail.com>
2022-11-14 18:07:09 +0300

    swaped on long-polling and test bot

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils.executor import start_webhook
 import config
 import os
 
 #env vars
-TOKEN = os.getenv('BOT_TOKEN')
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+TOKEN = os.getenv('TEST_BOT_TOKEN')
 
-#webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-
-#webserver setitings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
 bot = Bot(TOKEN)
@@ -23,12 +12,10 @@ dp = Dispatcher(bot)
 
 
 async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
+    pass
 
 async def on_shutdown(dp):
-    logging.warning('Shutting down..')
-    await bot.delete_webhook()
-
+    pass
 
 
 @dp.message_handler(commands=['help'])
@@ -48,12 +35,9 @@ async def echo(message: types.Message):
     await message.answer(text=message.text)
 
 if __name__ == '__main__':
-    start_webhook(
+    executor.start_polling(
         dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
     )
