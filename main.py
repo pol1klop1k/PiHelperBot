53b8f116400764647cf5bc485c8de967c9923a3a
unknown <garik.b.04@gmail.com>
2022-11-15 17:46:51 +0300

    added choise subject panel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 import config
 import os
 
@@ -26,13 +27,70 @@ async def help_command(message: types.Message):
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await message.delete()
-    await message.answer(text="Привет! Я бот помощник по учебному процессу.")
-    await message.answer(text='Чтобы увидеть список моих комманд пиши "/help"')
+    await message.answer(text="Привет! Я бот помощник по учебному процессу")
+
+    history = KeyboardButton(
+    text='История',
+    )
+
+    informatic = KeyboardButton(
+    text='Информатика',
+    )
+
+    #functional = ReplyKeyboardMarkup(
+    #resize_keyboard=True,
+    #)
+
+    functional = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    )
+
+    await message.answer(text='Вот список моих некоторых комманд', reply_markup=functional.row(history, informatic))
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
+@dp.message_handler(lambda message: message.text=='История')
+async def history(message: types.Message):
+
+    ikb = InlineKeyboardMarkup(row_width=5)
+    count = 57
+    a = iter(range(1,count+1))
+    for i in range(count//5+1):
+        butts = []
+        for k in a:
+            current = str(k)
+            butts.append(InlineKeyboardButton(text=current, callback_data=current))
+            if k % 5 == 0:
+                break
+        ikb.row(*butts)
+
+
+    await message.answer(
+    text='Какой вопрос по истории интересует?',
+    reply_markup=ikb,
+    )
+
+
+@dp.message_handler(lambda message: message.text=='Информатика')
+async def informatic(message: types.Message):
+
+    ikb = InlineKeyboardMarkup(row_width=5)
+    count = 77
+    a = iter(range(1,count+1))
+    for i in range(count//5+1):
+        butts = []
+        for k in a:
+            current = str(k)
+            butts.append(InlineKeyboardButton(text=current, callback_data=current))
+            if k % 5 == 0:
+                break
+        ikb.row(*butts)
+
+    await message.answer(
+    text='Какой вопрос по информатике интересует?',
+    reply_markup=ikb,
+    )
+
+
 
 if __name__ == '__main__':
     executor.start_polling(
